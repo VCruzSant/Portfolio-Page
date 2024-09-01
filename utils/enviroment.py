04fa500ff8531c7d5a499623227ebc163267f31f
VCruzSant <sviniantiago@gmail.com>
2024-09-01 15:07:54 -0300

    CSRF_TRUSTED_ORIGINS fix

--- utils/enviroment.py
+++ utils/enviroment.py
@@ -8,4 +8,4 @@ def get_env_variable(variable_name, default_value=''):
 def parse_comma_str_to_list(comma_set_str):
     if not comma_set_str or not isinstance(comma_set_str, str):
         return []
-    return [string for string in comma_set_str.split(',')]
+    return [string.strip() for string in comma_set_str.split(',') if string]
